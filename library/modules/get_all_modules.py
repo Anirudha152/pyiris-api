2918a09f5baa635d4a8a79c99b849ada5fe9e5b5
Anirudha152 <40232319+Anirudha152@users.noreply.github.com>
2021-01-28 15:25:47 +0530

    Delete library directory

--- library/modules/get_all_modules.py
+++
@@ -1,9 +0,0 @@
-# GUI + CUI
-# done
-import os
-
-
-def main(directory):
-    for dirpath, _, filenames in os.walk(directory):
-        for f in filenames:
-            yield os.path.abspath(os.path.join(dirpath, f))
--- library/modules/key_from_val.py
+++
@@ -1,7 +0,0 @@
-# GUI + CUI
-# done
-def main(dictionary, target):
-    for key, val in dictionary.items():
-        if val == target:
-            return key
-    return None
